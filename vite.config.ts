f9202ac5761c3b8adfed6e8c9309749cab81c774
sms1989 <mohammad.shobeiri@live.com>
2024-08-04 00:03:47 +0330

    chore: add pwa config

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,47 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), VitePWA({ registerType: "autoUpdate" })],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "جاسوس",
+        icons: [
+          {
+            src: "pwa-64x64.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "maskable-icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+        display: "standalone",
+        theme_color: "#000000",
+        categories: ["games", "entertainment"],
+        description: "بازی دسته جمعی جاسوس",
+        lang: "fa",
+        start_url: "/",
+        dir: "rtl",
+        orientation: "portrait",
+      },
+    }),
+  ],
   test: {
     globals: true,
     environment: "jsdom",
