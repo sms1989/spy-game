733c9143a50be82acbf300f63105fc4ce62262ca
sms1989 <mohammad.shobeiri@live.com>
2024-08-09 00:26:39 +0330

    style: fix height to svh

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -11,12 +11,12 @@ export default function DefaultLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="relative flex flex-col h-screen">
+    <div className="relative flex flex-col h-svh">
       <img src="/background.jpg" className="absolute left-1/2 -translate-x-1/2 object-cover max-w-sm w-full z-0" />
       <main className="container mx-auto max-w-sm px-6 flex-grow flex items-center z-10 pt-[400px]">
         {children}
       </main>
-      <footer className="w-full flex items-center max-w-sm py-3 h-20 justify-between mx-auto items-center">
+      <footer className="w-full flex max-w-sm py-3 h-20 justify-between mx-auto items-center">
         <Link isExternal href={siteConfig.links.github}>
           <GithubIcon className="text-default-500" />
         </Link>
